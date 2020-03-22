99ee5f6b478e9ec7f0e05abf1f557880c1f6fbc6
unknown <fayssaloudbib@gmail.com>
2020-03-22 20:28:45 +0100

    adding comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let toulouseLocation = {lat: 43.604390, lon: 1.443379};
-let map = L.map('mapid').setView(toulouseLocation, 19);
+let map = L.map('map').setView(toulouseLocation, 19);
+// let map = L.map('map');
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,4 +12,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.scale().addTo(map);
 
 // show a marker on the map
-L.marker(toulouseLocation).bindPopup('The center of the world').addTo(map);
+L.marker(toulouseLocation).bindPopup('Tolosa is the number one').addTo(map);
+
+/**
+ * Find current location
+ */
+
+// callback function on locationFound event
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
+//     L.marker(e.latlng).addTo(map)
+//         .bindPopup("You are within " + radius + " meters from this point").openPopup();
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+
+// map.on('locationfound', onLocationFound);
